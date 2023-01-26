e2afdbe16f50c16bf8744ae6f23678ea2d4abb2e
DeVon DuPree <devondupree@Air-DeVonny-Pooh.local>
2023-01-25 16:32:07 -0800

    first commit

---
+++ ITM/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from './assets/logo.png'
+
+const App = () => {
+  return (
+    <div className="font-ptsans">
+      {/* <h1 className="font-bold">ITM</h1> */}
+      <img src={logo} alt="" />
+    </div>
+  )
+}
+
+export default App
