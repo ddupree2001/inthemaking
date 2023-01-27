ef928336f0f69739180c1a3783c090bab87276f5
DeVon DuPree <devondupree@Air-DeVonny-Pooh.local>
2023-01-27 13:25:57 -0800

    services

--- ITM/src/App.jsx
+++ ITM/src/App.jsx
@@ -3,18 +3,49 @@ import logo from './assets/logo.png'
 import redTriangle from './assets/redTriangle.png'
 import hero from './assets/hero.png'
 import workoutplan from './assets/workoutplan.png'
+import instagram from './assets/instagram.png'
 import { AwesomeButton } from "react-awesome-button";
 import 'react-awesome-button/dist/styles.css';
 import {FiMenu} from 'react-icons/fi'
+import {AiOutlineLeft, AiOutlineRight} from 'react-icons/ai'
+import { motion } from "framer-motion"
 import './App.css'
 
 const App = () => {
 
-  const highlights = [
-    'Dreams',
-    'Personalized',
-    'Results',
-    'Begin'
+  const testimonials = [
+    {
+      name: "Blajon L.",
+      quote: "I've known Jadon Garcia (In The Making), for many years now. One Thing about him is he knows how to stay consistent and motivate the people around him. I've trained along side with him since we grew up as kids, but since becoming a personal trainer he took my body to new levels that I couldn't reach by myself. He's very polite and makes you feel extremely comfortable (no matter your fitness level). In The Making is the right fit for you if you want to reach your goals and take yourself to the next level. He's a one of a kind trainer, I truly recommend you to book your session today and see for yourself."
+    },
+    {
+      name: "David S.",
+      quote: "I started working out with Jadon in May 2021 through 1:1 and small group training. When I started I was about 125lbs and have gained significant strength and size within our time frame of training. Im currently close to my goal weight, along with improved lean mass and noticeably different physique. The workouts are motivating and challenging, with different workouts each session. Jadon has also helped me with nutritional advice that has been helping me throughout this process of reaching my goals."
+    },
+    {
+      name: "Melissa R.",
+      quote: "Jadon is the truth! If you're looking for a trainer who listens to your fitness goals and specifies your workout towards theme's the trainer for you! He will push you to be the best version of yourself in every workout! Super friendly and knowledgable. Highly recommend him."
+    },
+    {
+      name: "Chris & Carrie T.",
+      quote: "Jadon is an amazing trainer and all-around great person. We highly recommend him. My daughter is a D1 soccer player in college and my son is also a competitive soccer player in highschool. Jadon is the go-to trainer for both. As a former college football player and high level athlete himself, Jadon understands exactly the training athletes need to optimize performance. The workouts are tailored to their specific need. Jadon's energy is infectious and motivating. My son loves training with him and Jadon is able to get the most out of him. He was also great for my daughter in her recovery from injury. He has a gift of connecting with all his clients."
+    },
+    {
+      name: "Diana",
+      quote: "I've been training with Jadon for over a year now and can say it's been so much fun! He has helped me build more strength and muscle endurance more than ever before. I also feel more efficient with my movements on the court which has been a game changer to me. As an athlete that values putting in the hard work to achieve my goals, I really appreciate a trainer that has the same mindset and is always working to improve himself as well. He is so encouraging and really pushes you to challenge your mental and physical abilities, so that you can be better as an athlete while achieving whatever physical goals you have. He does an awesome job of mixing in compound exercises with a lot of speed and agility drills too, so you'll have so much fun training with him if you want to get stronger and move better. I've also had a few injuries from volleyball over the year and Jadon was so patient with me, still allowing me to train around that and we were able to incorporate rehab exercises to support my injuries. He's super knowledgable and has a library of exercises in his head so you're definitely in good hands. I'll add that Jadon is also so kind, funny and caring. You'll get your work in but also so kind, funny and caring. You'll get your work in but also feel like you have a friend to talk to and push you through all the hard stuff. Whether you're n athlete or a weekday warrior, you won't regret having him as your trainer."
+    },
+    {
+      name: "David P.",
+      quote: "I've tried other trainers. I even did Crossfit for 9 months but Jadon is my all time favorite training experience. Hands down. I trained with him from September of 2020 to November of 2021 so just a little over a year. I started at 270 lbs and ended up at 211 lbs. That was nearly a 60 lbs loss in a little over a year. I actually had the first signs of a six pack for the first time in my life. That felt amazing! I decided to take on a killer combo of practicing Jiu Jitsu twice a week, running on the off days and keeping a strict diet and then training with Jadon twice a week and the results were fast and amazing. I credit Jadon and his program and his consistent affirmation and encouragement with helping me build incredible core strength and a solid body that helped me get there and maintain a robust metabolism to help burn that fat. What I really appreciated about Jadon was his uncanny ability to look at me and judge where I was at and what I was capable of and then assign workouts to me that were just enough to provide me with a great workout but still be on the upper end of my abilities and push me to go a little farther than I thought I could do. He didn’t set insurmountable milestones for me that made me feel inferior or defeated like I often felt at Crossfit. So many times he would assign me an exercise with a specific weight and a specific number of reps and it would turn out to be just exactly what I could handle with a need to really push on the last reps. It was like he was great at finding the workout sweet spot. I was impressed that he could call that out time and time again. I used to bring my son a lot to the workouts too and Jadon made it a fun and bonding time for us. I was very impressed with his wide range of exercises that he was familiar with and how he consistently switched things up during different workouts to keep things interesting and engaging. He even incorporated boxing for me and my son and made it fun. That was wicked cardio too!! I liked how Jadon called out the athlete in both of us and provided lots of affirmation. I would recommend to anybody who wants to get results to train with Jadon."
+    },
+    {
+      name: "Lorenzo",
+      quote: "Jadon helped me ease into working out again, after a long break due to covid. He breaks it down for you to simplify the workouts, making it an enjoyable experience."
+    },
+    {
+      name: "Valerie",
+      quote: "I have been training with Jadon for a little over a year now, we started on October 18, 2021. My siblings were encouraging me to start working out, but I knew I would need someone to hold me accountable. Jadon has been just that! He works well with your schedule and still keeps a stern stance one the goals we both have set for myself. He is always making sure that I stick to what I said and I really need that accountability reminder. He also listens to you and your body during the sessions so if there's any adjustments that need to be made, he will alter the workout to fit my needs. He's never too pushy, but pushy enough. Y'all get Jadon in your life!"
+    }
   ]
 
   return (
@@ -27,9 +58,9 @@ const App = () => {
             <img src={logo} alt="" className="z-10 w-[150px] sm:w-[250px]" />
             <ul className="z-10 justify-between gap-x-[80px] text-white hidden lg:flex">
               <li><a href="#about">About</a></li>
-              <li><a>Services</a></li>
-              <li><a>Testimonials</a></li>
-              <li><a>Contact</a></li>
+              <li><a href="#services">Services</a></li>
+              <li><a href="#testimonials">Testimonials</a></li>
+              <li><a href="#contact">Contact</a></li>
             </ul>
             <FiMenu className="text-white w-[30px] h-[30px] block lg:hidden cursor-pointer"/>
           </nav>  
@@ -41,7 +72,7 @@ const App = () => {
                 {/* <button className="text-white border-2 border-[#caab69] rounded-md px-[20px] py-[15px] shadow-[0px_4px_4px_#bfa264]">
                   Schedule With Me!
                 </button> */}
-                <AwesomeButton type="primary" ripple href="/" target="_self">Schedule With Me!</AwesomeButton>
+                <AwesomeButton type="primary" ripple href="#services" target="_self">Schedule With Me!</AwesomeButton>
               </div>
             </div>
             <img src={hero} alt="" className="w-[40%] hidden  md:block xl:hidden" />
@@ -62,6 +93,87 @@ const App = () => {
             <img src={workoutplan} alt="workout plan" className="w-[40%] max-w-[381px]"/>
           </div>
         </div>
+        <div className="relative px-[50px] py-[20px] section" id="services">
+          <div className="flex flex-col items-center justify-center gap-y-[4vh]">
+            <h2 className="text-white text-center font-bold text-[1.75rem] sm:text-[3rem]">Transform Your Body and Mind with <span className="text-[#caab69]">Personalized </span> Training</h2>
+            <p className="text-white text-center text-[1.125rem] sm:text-[1.25rem] w-full max-w-[700px]">I offer a wide range of personal training, performance training and recovery sessions. Each session can be tailored to your individual goals and needs.</p>
+          </div>
+          <div className="grid grid-flow-col grid-rows-4 md:grid-rows-2 xl:grid-rows-1 justify-center mt-[8vh] gap-y-10 gap-5 xl:gap-0">
+            <motion.div whileHover={{scale: 1.1}} className="relative">
+              <svg width="325" height="325" viewBox="0 0 342 368" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M3 345V22.5C3 11.4543 11.9543 2.5 23 2.5H257.979C263.423 2.5 268.632 4.71922 272.403 8.64541L333.924 72.6948C337.502 76.4197 339.5 81.3844 339.5 86.5494V345C339.5 356.046 330.546 365 319.5 365H23C11.9543 365 3 356.046 3 345Z" stroke="#892827" stroke-width="5"/>
+              </svg>
+              <p className="absolute text-white font-bold text-[2rem] left-[40px] top-[20px]">Personal</p>
+              <p className="absolute text-white text-[1.25rem] left-[60px] top-[80px]">1 on 1 - $100/session</p>
+              <p className="absolute text-white text-[1.25rem] left-[60px] top-[130px]">Small Group - $60/session</p>
+              <p className="absolute text-white text-[1.25rem] left-[60px] top-[180px]">Large Group - $100/session</p>
+              <AwesomeButton type="danger" ripple href="#services" target="_self" className="absolute left-[60px] top-[235px]">Let's Get To Work!</AwesomeButton>
+            </motion.div>
+            <motion.div whileHover={{scale: 1.1}} className="relative">
+              <svg width="325" height="325" viewBox="0 0 342 368" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M3 345V22.5C3 11.4543 11.9543 2.5 23 2.5H257.979C263.423 2.5 268.632 4.71922 272.403 8.64541L333.924 72.6948C337.502 76.4197 339.5 81.3844 339.5 86.5494V345C339.5 356.046 330.546 365 319.5 365H23C11.9543 365 3 356.046 3 345Z" stroke="#892827" stroke-width="5"/>
+              </svg>
+              <p className="absolute text-white font-bold text-[2rem] left-[40px] top-[20px]">Performance</p>
+              <p className="absolute text-white text-[1.25rem] left-[60px] top-[80px]">1 on 1 - $100/session</p>
+              <p className="absolute text-white text-[1.25rem] left-[60px] top-[130px]">Small Group - $55/session</p>
+              <p className="absolute text-white text-[1.25rem] left-[60px] top-[180px]">Large Group - <span className="text-[#892827]">X</span></p>
+              <AwesomeButton type="danger" ripple href="#services" target="_self" className="absolute left-[60px] top-[235px]">Let's Get To Work!</AwesomeButton>
+            </motion.div>
+            <motion.div whileHover={{scale: 1.1}} className="relative">
+              <svg width="325" height="325" viewBox="0 0 342 368" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M3 345V22.5C3 11.4543 11.9543 2.5 23 2.5H257.979C263.423 2.5 268.632 4.71922 272.403 8.64541L333.924 72.6948C337.502 76.4197 339.5 81.3844 339.5 86.5494V345C339.5 356.046 330.546 365 319.5 365H23C11.9543 365 3 356.046 3 345Z" stroke="#892827" stroke-width="5"/>
+              </svg>
+              <p className="absolute text-white font-bold text-[2rem] left-[40px] top-[20px]">Recovery</p>
+              <p className="absolute text-white text-[1.25rem] left-[60px] top-[80px]">1 on 1 - $50/session</p>
+              <p className="absolute text-white text-[1.25rem] left-[60px] top-[130px]">Small Group - <span className="text-[#892827]">X</span></p>
+              <p className="absolute text-white text-[1.25rem] left-[60px] top-[180px]">Large Group - <span className="text-[#892827]">X</span></p>
+              <AwesomeButton type="danger" ripple href="#services" target="_self" className="absolute left-[60px] top-[235px]">Let's Get To Work!</AwesomeButton>
+            </motion.div>
+            <motion.div whileHover={{scale: 1.1}} className="relative">
+              <svg width="325" height="325" viewBox="0 0 342 368" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M3 345V22.5C3 11.4543 11.9543 2.5 23 2.5H257.979C263.423 2.5 268.632 4.71922 272.403 8.64541L333.924 72.6948C337.502 76.4197 339.5 81.3844 339.5 86.5494V345C339.5 356.046 330.546 365 319.5 365H23C11.9543 365 3 356.046 3 345Z" stroke="#892827" stroke-width="5"/>
+              </svg>
+              <p className="absolute text-white font-bold text-[2rem] left-[40px] top-[20px]">Speed</p>
+              <p className="absolute text-white text-[1.25rem] left-[60px] top-[80px]">1 on 1 - $40/session</p>
+              <p className="absolute text-white text-[1.25rem] left-[60px] top-[130px]">Small Group - <span className="text-[#892827]">X</span></p>
+              <p className="absolute text-white text-[1.25rem] left-[60px] top-[180px]">Large Group - <span className="text-[#892827]">X</span></p>
+              <AwesomeButton type="danger" ripple href="#services" target="_self" className="absolute left-[60px] top-[235px]">Let's Get To Work!</AwesomeButton>
+            </motion.div>
+          </div>
+        </div>
+        <div className="relative px-[50px] py-[20px] section" id="testimonials">
+          <div className="flex justify-center mb-[5vh]">
+            <h2 className="text-white text-center font-bold text-[1.75rem] sm:text-[3rem]">Real <span className="text-[#caab69]">Results</span> from Real People</h2>
+          </div>
+          <div className="relative flex justify-center">
+            <div className="rounded-md w-[80vw] md:w-[70vw] max-w-[1200px] flex overflow-x-auto overflow-y-auto h-[300px] bg-[#caab69]">
+              {testimonials.map((testimonial, index) => (
+                <div className="relative flex-[0_0_100%] px-16 py-8" id={index} key={index}>
+                  <p className="text-[1.25rem] sm:text-[1.5rem] font-bold mb-[2vh]">{testimonial.name}</p>
+                  <p className="text-[1.125rem] sm:text-[1.25rem]">{testimonial.quote}</p>
+                </div>
+              ))}
+            </div>
+                  <AiOutlineLeft className="text-[#892827] absolute top-[50%] left-[5%] md:left-[12.5%] mt-[-15px] sm:mt-[-25px] w-[25px] h-[25px] sm:w-[50px] sm:h-[50px] cursor-pointer" />
+                  <AiOutlineRight className="text-[#892827] absolute top-[50%] right-[5%] md:right-[12.5%] mt-[-15px] sm:mt-[-25px] w-[25px] h-[25px] sm:w-[50px] sm:h-[50px] cursor-pointer"  />
+          </div>
+        </div>
+        {/* <div className="relative px-[50px] py-[20px] section" id="contact">
+          <div className="flex justify-center">
+            <h2 className="text-white text-center font-bold text-[1.75rem] sm:text-[3rem]">Let's <span className="text-[#caab69]">Begin</span> Your Journey Together</h2>
+          </div>
+          <div className="flex justify-center items-center gap-x-20 mt-[5vh]">
+            <img src={instagram} alt="" />
+            <div className="flex flex-col gap-y-[5vh]">
+              <p className="text-white font-bold text-[2rem]">Phone: (408) 489-7662</p>
+              <p className="text-white font-bold text-[2rem]">Email: j.don95@outlook.com</p>
+              <p className="text-white font-bold text-[2rem]">Instagram: @inthemaking.pt</p>
+              <div>
+                <AwesomeButton type="secondary" ripple href="#services" target="_self">Book A Session!</AwesomeButton>
+              </div>
+            </div>
+          </div>
+        </div> */}
       </div>
     </div>
   )
